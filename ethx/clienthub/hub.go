cfd8f0af0017d8fabc3130c1039148fdc977556c
lrhaoo <lrhhhhhh@gmail.com>
2024-11-26 10:13:49 +0800

    refactor: Change field 'chains' to 'Chains' for external accessibility

--- ethx/clienthub/hub.go
+++ ethx/clienthub/hub.go
@@ -9,18 +9,18 @@ import (
 
 // ClientHub is a multi-chain client manager
 type ClientHub struct {
-	chains  map[int]chain.IConfig
+	Chains  map[int]chain.IConfig
 	clients map[int]*client.Client
 }
 
 func New(chains map[int]chain.IConfig) (*ClientHub, error) {
 	hub := &ClientHub{
-		chains:  make(map[int]chain.IConfig),
+		Chains:  make(map[int]chain.IConfig),
 		clients: make(map[int]*client.Client),
 	}
 
 	for chainId, chainConfig := range chains {
-		hub.chains[chainId] = chainConfig
+		hub.Chains[chainId] = chainConfig
 		client, err := client.NewClient(chainConfig)
 		if err != nil {
 			return nil, errors.Wrap(err, "fail to create client")
