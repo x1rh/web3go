17b9975de6dc76e81e7464e7bb355f2545940319
lrhaoo <lrhhhhhh@gmail.com>
2024-11-27 19:25:03 +0800

    feat: add castx.ToAddress() and castx.MustToAddress()

--- ethx/castx/address.go
+++ ethx/castx/address.go
@@ -1,7 +1,27 @@
 package castx
 
-import "github.com/ethereum/go-ethereum/common"
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"errors"
+)
 
 func ToAddress(iaddress any) (*common.Address, error) {
-	return nil, nil 
+	var address common.Address
+    switch v := iaddress.(type) {
+    case string:
+        address = common.HexToAddress(v)
+		return &address, nil 
+    case common.Address:
+		return &v, nil 
+    default:
+        return nil, errors.New("invalid address type")
+    }
+}
+
+func MustToAddress(iaddress any) *common.Address {
+	address, err := ToAddress(iaddress)
+	if err != nil {
+		panic(err)
+	}
+	return address
 }
--- ethx/castx/address_test.go
+++ ethx/castx/address_test.go
@@ -1,1 +1,14 @@
 package castx
+
+import (
+	"testing"
+)
+
+func TestToAddress(t *testing.T) {
+	address1 := "0x13CB6AE34A13a0977F4d7101eBc24B87Bb23F0d5"
+	address2, err := ToAddress(address1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(address2)
+}
